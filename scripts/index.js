74b7a8c7154adc70443752488f0a4602fdc91982
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-05 15:58:22 -0500

    Adds kitty and bunny buttons and basic styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,28 @@
-// I have made some changes to this branch. Aylor is great!!!
+
+
+// When you use class names to reference DOM elements, *always* prefix with "js-"
+// This signals to everyone else to not change that class name because it breaks everything js
+
+const thePic = document.querySelector(".js-picture");
+const bunnyButton = document.querySelector(".js-bunny");
+const kittyButton = document.querySelector(".js-kitty");
+
+
+function changePic(aPic, imgPath) {
+    aPic.src = imgPath;
+}
+
+function hello() {
+    console.log("it works");
+}
+
+function showBunny() {
+    changePic(thePic, "images/bunny.jpg");
+}
+
+function showKitty() {
+    changePic(thePic, "images/kitty.jpg");
+}
+
+bunnyButton.addEventListener("click", showBunny);
+kittyButton.addEventListener("click", showKitty);

09a58553c2deed6a2e1c6a93752940459d874809
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-06 14:52:13 -0500

    Adds Dom element functions

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,32 @@ function showKitty() {
 
 bunnyButton.addEventListener("click", showBunny);
 kittyButton.addEventListener("click", showKitty);
+
+// 1. A reference to the .js-photo-container
+const photoContainer = document.querySelector(".js-photo-container");
+// 2. A function that can accept an image path string and return an img DOM element, whose source points to that img path
+function imgWithPath(imgPath) {
+    // create an image element
+    const img = document.createElement("img");
+    // set its path
+    img.src = imgPath;
+    // alternatively, we could call changePic
+    // changePic(img, imgPath);
+    // return the element
+    return img;
+}
+
+function appendImageToContainer(imgEl) {
+    photoContainer.appendChild(imgEl);
+}
+// 3.
+
+const images = [
+    "images/bunny.jpg",
+    "images/kitty.jpg"
+];
+
+function displayImages(imgArray) {
+    let imgsWSrc = imgArray.map(imgWithPath);
+    imgsWSrc.map(appendImageToContainer);
+}
